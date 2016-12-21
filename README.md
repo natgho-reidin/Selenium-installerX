d0a0530439ec7b0785176622fc63a8448acf196b
natgho-reidin <sbozkir@reidin.com>
2016-12-21 16:58:01 +0300

    Complete Code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Selenium-installerX
+Automatic Selenium and tools installer bash script. (for 64 bit)
+
+#How to Use?
+$ sudo chmod +x seleniumInstaller.sh
+$ sudo ./seleniumInstaller.sh
---
+++ seleniumInstaller32bit .sh
@@ -0,0 +1,27 @@
+sudo apt-get install python-pip
+sudo apt-get install unzip
+sudo pip install -U selenium
+
+# Create all file for drivers
+mkdir /home/$USER/.seleniumdrivers
+cd /home/$USER/.seleniumdrivers
+
+# Download and configure mozilla driver
+wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux32.tar.gz --no-check-certificate
+tar -xvzf geckodriver-v0.11.1-linux32.tar.gz
+rm geckodriver-v0.11.1-linux32.tar.gz
+chmod +x geckodriver
+
+# download and configure chrome driver
+wget https://chromedriver.storage.googleapis.com/2.26/chromedriver_linux32.zip
+unzip chromedriver_linux32.zip
+rm chromedriver_linux32.zip
+chmod +x chromedriver
+
+# Add file linux path
+echo "# Selenium Drivers Path" >> /home/$USER/.bashrc
+echo "PATH=$PATH:/home/$USER/.seleniumdrivers" >> /home/$USER/.bashrc
+
+clear
+echo "Installiation Complete :)"
+
