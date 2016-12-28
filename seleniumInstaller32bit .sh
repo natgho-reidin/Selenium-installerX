573eba5eec247035e70aa87c8731c677c0bfae19
natgho-reidin <sbozkir@reidin.com>
2016-12-28 19:15:20 +0300

    Bug fixed

--- seleniumInstaller32bit .sh
+++ seleniumInstaller32bit .sh
@@ -1,19 +1,27 @@
-sudo apt-get install python-pip
-sudo apt-get install unzip
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y python-pip
+sudo apt-get install -y python3-pip
+sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
+sudo apt-get install -y unzip
 sudo pip install -U selenium
 sudo -H pip install --upgrade selenium
-sudo apt-get install python3-selenium
+sudo apt-get install -y python-setuptools
+sudo pip3 install selenium
+sudo pip3 install --upgrade selenium
 
 # Install chromium browser
-sudo apt-get install chromium-browser
+sudo apt-get install -y chromium-browser
 
 # work for servers install pyvirtualdisplay module and extras...
-sudo apt-get install xvfb
-sudo -H pip install pyvirtualdisplay
+sudo apt-get install -y xvfb
+sudo easy_install pyvirtualdisplay
+sudo pip3 install pyvirtualdisplay
 
 # Geckodriver not running with firefox 50.0.1, so should be install downgrade 45.0.2 version
 sudo apt-get remove firefox
-sudo apt-get install firefox=45.0.2+build1-0ubuntu1
+sudo apt-get install -y firefox=45.0.2+build1-0ubuntu1
 
 # Hold firefox version
 sudo apt-mark hold firefox
@@ -25,7 +33,6 @@ sudo apt-mark hold firefox
 mkdir /opt/seleniumdrivers
 chmod 755 /opt/seleniumdrivers
 cd /opt/seleniumdrivers
-
 # Download and configure mozilla driver
 wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux32.tar.gz --no-check-certificate
 tar -xvzf geckodriver-v0.11.1-linux32.tar.gz
@@ -41,9 +48,8 @@ unzip chromedriver_linux32.zip
 rm chromedriver_linux32.zip
 chmod +x chromedriver
 
-# Add file linux path except root
-ln -s /opt/seleniumdrivers /usr/bin/selenium
+# Add selenium files linux path 
+echo "export PATH=$PATH:/opt/seleniumdrivers" >> /etc/profile
 
 clear
-echo "Installiation Complete :)"
-
+echo "Installiation Complete :) Please restart system."
